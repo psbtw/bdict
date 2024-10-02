5ce5e70cbb2ede75970515ed70303161a6fbf664
psbtw <billchen965@gmail.com>
2024-10-02 23:55:33 +0800

    fix compile

--- pinyinEncoder/parser.cpp
+++ pinyinEncoder/parser.cpp
@@ -2,14 +2,37 @@
 
 void PinyinParser::init_base_map()
 {
-    unordered_map<string, vector<PinyinSyllable>> base_map = {
+    _base_map = move(unordered_map<string, vector<PinyinAlphabet>>{
         {"a", {PinyinAlphabet::A}},
-        {"o", {PinyinAlphabet::O}}
-    };
+        {"o", {PinyinAlphabet::O}},
+        {"e", {PinyinAlphabet::E}},
+        {"i", {PinyinAlphabet::I}},
+        {"u", {PinyinAlphabet::U}},
+        {"v", {PinyinAlphabet::V}},
+        {"uang", {PinyinAlphabet::UANG}},
+    });
 }
 
-std::vector<PinyinAlphabet> PinyinParser::StringToAlphabet(std::string &str)
+vector<vector<PinyinAlphabet>> PinyinParser::StringToAlphabet(std::string &str)
 {
-    std::vector<PinyinAlphabet> ret;
+    vector<vector<PinyinAlphabet>> ret; //n*m fuzzy combinations
+
+    //check from initial to initial
+    
+
+
     return ret;
+}
+
+PinyinParser::PinyinParser()
+{
+     _base_map = move(unordered_map<string, vector<PinyinAlphabet>>{
+        {"a", {PinyinAlphabet::A}},
+        {"o", {PinyinAlphabet::O}},
+        {"e", {PinyinAlphabet::E}},
+        {"i", {PinyinAlphabet::I}},
+        {"u", {PinyinAlphabet::U}},
+        {"v", {PinyinAlphabet::V}},
+        {"uang", {PinyinAlphabet::UANG}},
+    });
 }
