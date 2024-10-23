f8989dc5f9ac5cdc268b4098da7348a6a757b122
psbtw <billchen965@gmail.com>
2024-10-23 23:40:30 +0800

    set fuzzy map

--- pinyinEncoder/parser.cpp
+++ pinyinEncoder/parser.cpp
@@ -35,4 +35,35 @@ PinyinParser::PinyinParser()
         {"v", {PinyinAlphabet::V}},
         {"uang", {PinyinAlphabet::UANG}},
     });
+
+    _fuzzy_set = move(unordered_map<PinyinFuzzyFlag, unordered_map<string, vector<PinyinAlphabet>>> {
+        {
+            PinyinFuzzyFlag::AN_ANG, 
+            {
+                {"an",{PinyinAlphabet::AN, PinyinAlphabet::ANG}},
+                {"ang",{PinyinAlphabet::AN, PinyinAlphabet::ANG}},
+            }
+        },
+        {
+            PinyinFuzzyFlag::L_N, 
+            {
+                {"l",{PinyinAlphabet::L, PinyinAlphabet::N}},
+                {"n",{PinyinAlphabet::L, PinyinAlphabet::N}},
+            }
+        },
+
+    });
+}
+
+void PinyinParser::SetFuzzyFlags(vector<PinyinFuzzyFlag> &v) {
+    for (auto flag : v) {
+        if (_fuzzy_set.count(flag)) {
+            for (auto s : _fuzzy_set[flag]) {
+                _fuzzy_map[s.first] = s.second;
+            }
+        } else {
+            // not pre-defined
+        }
+
+    }
 }
--- pinyinEncoder/parser.hpp
+++ pinyinEncoder/parser.hpp
@@ -7,13 +7,12 @@ class PinyinParser {
 private:
     unordered_map<string, vector<PinyinAlphabet>> _base_map; //always there
     unordered_map<string, vector<PinyinAlphabet>> _fuzzy_map; //added optionally
-    unordered_map<PinyinFuzzyFlag, vector<PinyinAlphabet>> _fuzzy_set; //predefined
+    unordered_map<PinyinFuzzyFlag, unordered_map<string, vector<PinyinAlphabet>>> _fuzzy_set; //predefined
 
     void init_base_map();
 
 public:
     vector<vector<PinyinAlphabet>> StringToAlphabet(std::string& str);
     PinyinParser();
-
+    void SetFuzzyFlags(vector<PinyinFuzzyFlag> &v);
 };
-
