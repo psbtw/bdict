fff49cba030971d0731b810d8ec9ad7e5c2c589a
psbtw <billchen965@gmail.com>
2025-02-10 21:44:41 +0800

    bool init

--- hashMapDict.hpp
+++ hashMapDict.hpp
@@ -47,7 +47,7 @@ public:
     HashMapDict* Find(const std::vector<K>& key, int&);
     bool Insert(const std::vector<K>& key, const V&& data);
     bool InsertFirstN(const std::vector<K>& key, const V&& data, int n);
-    void BuildDict(const string&);
+    bool BuildDict(const string&);
     bool BuildDictWithFd(int fd,long offset,long length);
 
     bool LookUpByAlphabet(const Pinyin::PinyinVec& p, SortedVector<V>& res);
